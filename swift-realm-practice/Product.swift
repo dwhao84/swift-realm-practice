ef0382ca12cf345286772af4d301373449e0a3b0
Dawei Hao <dwsamurai84@gmail.com>
2025-03-29 00:08:03 +0800

    Rename the file.

--- swift-realm-practice/Product.swift
+++ swift-realm-practice/Product.swift
@@ -8,13 +8,16 @@
 import UIKit
 import RealmSwift
 
+// MARK: - 定義Product 為 RealmSwiftObject Obeject的類別。
 class Product: Object {
+    // MARK: -  將 @Persisted 定義為持久變數的屬性
     @Persisted(primaryKey: true) var id = UUID().uuidString
     @Persisted var articleNumber: String = ""
     @Persisted var location: String = ""
     @Persisted var qty: Int = 0
     @Persisted var timestamp: Date = Date()
     
+    // MARK: - init初始化這些內容，目前設定為 貨號、地點、數量、時間。
     convenience init(articleNumber: String, location: String, qty: Int) {
         self.init()
         self.articleNumber = articleNumber
